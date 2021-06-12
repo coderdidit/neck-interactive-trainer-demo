e08cd0af9e216ee6cd20d3b497b93568c00a91b0
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-06-12 12:50:55 +0100

    adding bootstrap

---
+++ src/sketch.js
@@ -0,0 +1,10 @@
+
+function setup() {
+    console.log('setup p5js')
+    var sketchCanvas =createCanvas(500, 500);
+    sketchCanvas.parent("nasit-canvas");
+  }
+
+  function draw() {
+    background(0);
+  }
