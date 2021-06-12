00843e69a8fb76c99242907a05e3665c26d05e15
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-06-12 14:54:59 +0100

    fix

--- src/sketch.js
+++ src/sketch.js
@@ -1,35 +1,11 @@
 let x, y;
 
-let speed = 0.1
-
-function updateBallPos() {
-  // Jiggling randomly on the horizontal axis
-  x = x + random(-1, 1);
-  // Moving up at a constant speed
-  y = y - 1;
-
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
-  }
-}
-
-function showBall() {
-  // fill(0, 153, 0);
-  noFill();
-  ellipse(x, y, 24, 24);
-}
+let speed = 0.4
 
 function setup() {
-  console.log('setup p5js')
+  console.log('setup p5js sketch')
   var sketchCanvas = createCanvas(900, 900);
   sketchCanvas.parent("nasit-canvas");
-  noStroke();
-  background(102, 178, 255);
-
-  // ladder
-  fill(127, 0, 255);
-  rect(400, 0, 50, 900);
 
   // init ball
   x = width / 2;
@@ -37,6 +13,26 @@ function setup() {
 }
 
 function draw() {
-  showBall()
-  updateBallPos()
+  // background
+  noStroke();
+  background(102, 178, 255);
+
+  // ladder
+  fill(127, 0, 255);
+  rect((width / 2)-25, 0, 60, 900);
+
+  // ball
+  stroke(50);
+  fill(0, 153, 0);
+  ellipse(x, y, 35, 35);
+
+  // Jiggling randomly on the horizontal axis
+  x = x + random(-1, 1);
+  // Moving up at a constant speed
+  y -= speed;
+  
+  // Reset to the bottom
+  if (y < 0) {
+    y = height;
+  }
 }
