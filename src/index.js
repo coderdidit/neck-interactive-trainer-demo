5a13b334573663343313b400b7af69b5471d5e32
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-08-08 23:00:19 +0100

    choose camera working

--- src/index.js
+++ src/index.js
@@ -16,213 +16,219 @@ const video = document.getElementById('video')
 const canvas = document.getElementById('video-output')
 
 let model, ctx
-let currentStream
+let currentStream, vidDevices
 
-const selectCamera = document.getElementById('cameras-options'); 
+const selectCamera = document.getElementById('cameras-options');
 const changeCameraBtn = document.getElementById('change-camera-btn');
 
-function gotDevices(mediaDevices) {
-    selectCamera.innerHTML = '';
-    selectCamera.appendChild(document.createElement('option'));
-    let count = 1;
-    mediaDevices.forEach(mediaDevice => {
-      if (mediaDevice.kind === 'videoinput') {
-        console.log('mediaDevice', mediaDevice)
-        const option = document.createElement('option');
-        option.value = mediaDevice.deviceId;
-        const label = mediaDevice.label || `Camera ${count++}`;
-        const textNode = document.createTextNode(label);
-        option.appendChild(textNode);
-        selectCamera.appendChild(option);
-      }
-    });
-  }
+function gotDevices() {
+  selectCamera.innerHTML = '';
+  selectCamera.appendChild(document.createElement('option'));
+  let count = 1;
+  vidDevices.forEach(mediaDevice => {
+    console.log('mediaDevice', mediaDevice)
+    const option = document.createElement('option');
+    option.value = mediaDevice.deviceId;
+    const label = mediaDevice.label || `Camera ${count++}`;
+    const textNode = document.createTextNode(label);
+    option.appendChild(textNode);
+    selectCamera.appendChild(option);
+  });
+}
 
 
 function stopMediaTracks(stream) {
-    console.log('stopMediaTracks')
-    stream.getTracks().forEach(track => {
-        track.stop();
-    });
+  console.log('stopMediaTracks')
+  stream.getTracks().forEach(track => {
+    track.stop();
+  });
 }
 
-changeCameraBtn.addEventListener('click', event => {
-    console.log('changeCameraBtn clicked', 'currentStream', currentStream)
-    if (typeof currentStream !== 'undefined') {
-      stopMediaTracks(currentStream);
-    }
-    const videoConstraints = {};
-    if (selectCamera.value === '') {
-      videoConstraints.facingMode = 'environment';
-    } else {
-      videoConstraints.deviceId = { exact: selectCamera.value };
-    }
-    const constraints = {
-      video: videoConstraints,
-      audio: false
-    };
-    
-    console.log('changing camera', constraints)
+changeCameraBtn.addEventListener('click', async (event) => {
+  console.log('changeCameraBtn clicked', 'currentStream', currentStream)
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream);
+  }
+  const videoConstraints = {};
+  if (selectCamera.value === '') {
+    videoConstraints.facingMode = 'environment';
+  } else {
+    videoConstraints.deviceId = { exact: selectCamera.value };
+  }
+  const constraints = {
+    video: videoConstraints,
+    audio: false
+  };
 
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(stream => {
-        currentStream = stream;
-        video.srcObject = stream;
-        return navigator.mediaDevices.enumerateDevices();
+  console.log('changing camera', constraints)
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      currentStream = stream;
+      video.srcObject = stream;
+      return new Promise((resolve) => {
+        video.onloadedmetadata = () => {
+          resolve(video)
+        }
       })
-      .then(gotDevices)
-      .catch(error => {
-        console.error(error);
-      });
-  });
-  
+    })
+    .then(() => {
+      startGame()
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
 
 const setupCamera = async () => {
-    console.log('setting up camera')
-    currentStream = await navigator.mediaDevices.getUserMedia({
-        'audio': false,
-        'video': { facingMode: 'user' }
-    })
-    video.srcObject = currentStream
-    console.log('currentStream', currentStream)
-    return new Promise((resolve) => {
-        video.onloadedmetadata = () => {
-            resolve(video)
-        }
-    })
+  console.log('setting up camera')
+  currentStream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': { facingMode: 'user' }
+  })
+  video.srcObject = currentStream
+  console.log('currentStream', currentStream)
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video)
+    }
+  })
 }
 
 const renderPrediction = async () => {
-    const returnTensors = false;
-    const flipHorizontal = false;
-    const annotateBoxes = true;
-    const predictions = await model.estimateFaces(
-        video, returnTensors, flipHorizontal, annotateBoxes)
+  const returnTensors = false;
+  const flipHorizontal = false;
+  const annotateBoxes = true;
+  const predictions = await model.estimateFaces(
+    video, returnTensors, flipHorizontal, annotateBoxes)
 
-    const videoWidth = video.videoWidth
-    const videoHeight = video.videoHeight
+  const videoWidth = video.videoWidth
+  const videoHeight = video.videoHeight
 
-    if (predictions.length > 0) {
-        // draw video to canvas 
-        ctx.drawImage(video, 0, 0, videoWidth, videoHeight)
-        /*
-        `predictions` is an array of objects describing each detected face, for example:
-    
-        [
-          {
-            topLeft: [232.28, 145.26],
-            bottomRight: [449.75, 308.36],
-            probability: [0.998],
-            landmarks: [
-              [295.13, 177.64], // right eye
-              [382.32, 175.56], // left eye
-              [341.18, 205.03], // nose
-              [345.12, 250.61], // mouth
-              [252.76, 211.37], // right ear
-              [431.20, 204.93] // left ear
-            ]
-          }
+  if (predictions.length > 0) {
+    // draw video to canvas 
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight)
+    /*
+    `predictions` is an array of objects describing each detected face, for example:
+ 
+    [
+      {
+        topLeft: [232.28, 145.26],
+        bottomRight: [449.75, 308.36],
+        probability: [0.998],
+        landmarks: [
+          [295.13, 177.64], // right eye
+          [382.32, 175.56], // left eye
+          [341.18, 205.03], // nose
+          [345.12, 250.61], // mouth
+          [252.76, 211.37], // right ear
+          [431.20, 204.93] // left ear
         ]
-        */
-        const prediction = predictions[0]
-        const start = prediction.topLeft;
-        const end = prediction.bottomRight;
-        const size = [end[0] - start[0], end[1] - start[1]];
-        ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+      }
+    ]
+    */
+    const prediction = predictions[0]
+    const start = prediction.topLeft;
+    const end = prediction.bottomRight;
+    const size = [end[0] - start[0], end[1] - start[1]];
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
 
-        if (annotateBoxes) {
-            const landmarks = prediction.landmarks;
+    if (annotateBoxes) {
+      const landmarks = prediction.landmarks;
 
-            const nose = landmarks[2]
-            const leftEye = landmarks[1]
-            const rightEye = landmarks[0]
+      const nose = landmarks[2]
+      const leftEye = landmarks[1]
+      const rightEye = landmarks[0]
 
-            const drawCircleAroundHead = () => {
-                ctx.beginPath();
-                ctx.arc(nose[0], nose[1], size[0] / 2, 0,
-                    2 * Math.PI, false);
-                ctx.fill()
-                ctx.stroke()
-            }
+      const drawCircleAroundHead = () => {
+        ctx.beginPath();
+        ctx.arc(nose[0], nose[1], size[0] / 2, 0,
+          2 * Math.PI, false);
+        ctx.fill()
+        ctx.stroke()
+      }
 
-            drawCircleAroundHead()
+      drawCircleAroundHead()
 
-            const drawLine = (p1, p2) => {
-                ctx.moveTo(p1[0], p1[1])
-                ctx.lineTo(p2[0], p2[1]);
-                ctx.stroke();
-            }
+      const drawLine = (p1, p2) => {
+        ctx.moveTo(p1[0], p1[1])
+        ctx.lineTo(p2[0], p2[1]);
+        ctx.stroke();
+      }
 
-            // path from nose to right eye
-            drawLine(nose, rightEye)
+      // path from nose to right eye
+      drawLine(nose, rightEye)
 
-            // path from nose to left eye
-            drawLine(nose, leftEye)
+      // path from nose to left eye
+      drawLine(nose, leftEye)
 
-            // path from nose to right end
-            drawLine(nose, [0, nose[1]])
+      // path from nose to right end
+      drawLine(nose, [0, nose[1]])
 
-            // path from nose to left end
-            drawLine(nose, [videoWidth, nose[1]])
+      // path from nose to left end
+      drawLine(nose, [videoWidth, nose[1]])
 
-            // calculate angles between 
-            // - line from nose to eye 
-            // - and straigh line from end to end crossing nose
-            // for left and right
-            const [noseToLeftEyeAngle, noseToRightEyeAngle] =
-                getAnglesBetween(nose, leftEye, rightEye)
+      // calculate angles between 
+      // - line from nose to eye 
+      // - and straigh line from end to end crossing nose
+      // for left and right
+      const [noseToLeftEyeAngle, noseToRightEyeAngle] =
+        getAnglesBetween(nose, leftEye, rightEye)
 
-            const activationAngle = 25
-            let landmarPointSize
-            if (noseToLeftEyeAngle < activationAngle
-                || noseToRightEyeAngle < activationAngle) {
-                window.gameStateMove()
-                ctx.fillStyle = "yellow";
-                landmarPointSize = 5
-            } else {
-                window.gameStateStop()
-                ctx.fillStyle = "blue";
-                landmarPointSize = 3
-            }
+      const activationAngle = 25
+      let landmarPointSize
+      if (noseToLeftEyeAngle < activationAngle
+        || noseToRightEyeAngle < activationAngle) {
+        window.gameStateMove()
+        ctx.fillStyle = "yellow";
+        landmarPointSize = 5
+      } else {
+        window.gameStateStop()
+        ctx.fillStyle = "blue";
+        landmarPointSize = 3
+      }
 
-            // draw face landmarks
-            // iterate up to 3 (right eye, left eye, nose)
-            for (let j = 0; j < 3; j++) {
-                const x = landmarks[j][0];
-                const y = landmarks[j][1];
-                ctx.beginPath();
-                ctx.arc(x, y, landmarPointSize, 0,
-                    2 * Math.PI, false);
-                ctx.fill()
-            }
-        }
+      // draw face landmarks
+      // iterate up to 3 (right eye, left eye, nose)
+      for (let j = 0; j < 3; j++) {
+        const x = landmarks[j][0];
+        const y = landmarks[j][1];
+        ctx.beginPath();
+        ctx.arc(x, y, landmarPointSize, 0,
+          2 * Math.PI, false);
+        ctx.fill()
+      }
     }
-    requestAnimationFrame(renderPrediction)
+  }
+  requestAnimationFrame(renderPrediction)
+}
+
+const startGame = async () => {
+  const videoWidth = video.videoWidth
+  const videoHeight = video.videoHeight
+  video.width = videoWidth
+  video.height = videoHeight
+
+  canvas.width = videoWidth
+  canvas.height = videoHeight
+
+  ctx = canvas.getContext('2d')
+  ctx.fillStyle = "rgba(255, 0, 0, 0.5)"
+
+  model = await blazeface.load()
+
+  renderPrediction()
 }
 
 const setupPage = async () => {
-    await tf.setBackend('wasm')
-    console.log('tfjs backend loaded')
-    navigator.mediaDevices.enumerateDevices().then(gotDevices);
-    const setupCameraRes = await setupCamera()
-    console.log('setupCamera finished', 'setupCameraRes', setupCameraRes)
-    video.play()
+  await tf.setBackend('wasm')
+  console.log('tfjs backend loaded')
 
-    const videoWidth = video.videoWidth
-    const videoHeight = video.videoHeight
-    video.width = videoWidth
-    video.height = videoHeight
-
-    canvas.width = videoWidth
-    canvas.height = videoHeight
-
-    ctx = canvas.getContext('2d')
-    ctx.fillStyle = "rgba(255, 0, 0, 0.5)"
-
-    model = await blazeface.load()
-
-    renderPrediction()
+  vidDevices = await navigator.mediaDevices.enumerateDevices()
+  vidDevices = vidDevices.filter(d => d.kind === 'videoinput')
+  gotDevices()
 }
 
 setupPage()
